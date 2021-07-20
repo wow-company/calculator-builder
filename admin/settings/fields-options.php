f6faf195c08de31fe43f637c7f8af6bd968dc5ee
Wow-Company <master@wow-company.com>
2021-07-20 13:41:16 +0300

    Added strings to translate

--- admin/settings/fields-options.php
+++ admin/settings/fields-options.php
@@ -9,22 +9,22 @@
     <div class="lightbox-content p-4">
         <a href="#" class="lightbox-close"><span class="dashicons dashicons-no-alt"></span> </a>
 
-        <div class="lightbox-title">Add Field</div>
+        <div class="lightbox-title"><?php esc_html_e( 'Add Field', 'calculator-builder' ); ?></div>
         <form action="" id="form-params" data-field-index="">
             <div class="columns">
                 <div class="column">
                     <div class="field">
-                        <label class="label">Field type</label>
+                        <label class="label"><?php esc_html_e( 'Field type', 'calculator-builder' ); ?></label>
                         <div class="control">
                             <div class="select is-link">
                                 <select name="type">
-                                    <option value="1">Number</option>
-                                    <option value="2">Select</option>
-                                    <option value="3">Radio</option>
-                                    <option value="4">Checkbox</option>
-                                    <option value="5">Number + Select</option>
-                                    <option value="6">Buttons</option>
-                                    <option value="7">Result</option>
+                                    <option value="1"><?php esc_attr_e( 'Number', 'calculator-builder' ); ?></option>
+                                    <option value="2"><?php esc_attr_e( 'Select', 'calculator-builder' ); ?></option>
+                                    <option value="3"><?php esc_attr_e( 'Radio', 'calculator-builder' ); ?></option>
+                                    <option value="4"><?php esc_attr_e( 'Checkbox', 'calculator-builder' ); ?></option>
+                                    <option value="5"><?php esc_attr_e( 'Number + Select', 'calculator-builder' ); ?></option>
+                                    <option value="6"><?php esc_attr_e( 'Buttons', 'calculator-builder' ); ?></option>
+                                    <option value="7"><?php esc_attr_e( 'Result', 'calculator-builder' ); ?></option>
                                 </select>
                             </div>
                         </div>
@@ -32,12 +32,12 @@
                 </div>
                 <div class="column type-number">
                     <div class="field">
-                        <label class="label">Required</label>
+                        <label class="label"><?php esc_html_e( 'Required', 'calculator-builder' ); ?></label>
                         <div class="control">
                             <div class="select is-link">
                                 <select name="required">
-                                    <option value="1">Yes</option>
-                                    <option value="2">No</option>
+                                    <option value="1"><?php esc_attr_e( 'ReYesquired', 'calculator-builder' ); ?></option>
+                                    <option value="2"><?php esc_attr_e( 'No', 'calculator-builder' ); ?></option>
                                 </select>
                             </div>
                         </div>
@@ -49,25 +49,27 @@
             <div class="columns">
                 <div class="column">
                     <div class="field">
-                        <label class="label">Title</label>
+                        <label class="label"><?php esc_html_e( 'Title', 'calculator-builder' ); ?></label>
                         <div class="control">
-                            <input class="input is-link" name="title" type="text" placeholder="Enter title" autocomplete="off" value="Label">
+                            <input class="input is-link" name="title" type="text" placeholder="Enter title"
+                                   autocomplete="off" value="Label">
                         </div>
                     </div>
                 </div>
                 <div class="column type-number type-result">
                     <div class="field">
-                        <label class="label">Addon</label>
+                        <label class="label"><?php esc_html_e( 'Addon', 'calculator-builder' ); ?></label>
                         <div class="field has-addons">
 
                             <div class="control is-expanded">
-                                <input class="input is-link" name="addon" type="text" placeholder="Enter addon" autocomplete="off">
+                                <input class="input is-link" name="addon" type="text" placeholder="Enter addon"
+                                       autocomplete="off">
                             </div>
                             <div class="control">
                                 <div class="select is-link">
                                     <select name="addon_pos">
-                                        <option>right</option>
-                                        <option>left</option>
+                                        <option value="right"><?php esc_attr_e( 'right', 'calculator-builder' ); ?></option>
+                                        <option value="left"><?php esc_attr_e( 'left', 'calculator-builder' ); ?></option>
                                     </select>
                                 </div>
                             </div>
@@ -80,21 +82,23 @@
                 <div class="column">
                     <div class="field has-addons">
                         <div class="control">
-                            <span class="button is-size-6 is-link">value</span>
+                            <span class="button is-size-6 is-link"><?php esc_html_e( 'value', 'calculator-builder' ); ?></span>
                         </div>
                         <div class="control is-expanded">
                             <input class="input is-link" name="value" type="text"
-                                   placeholder="Enter value or left empty" autocomplete="off">
+                                   placeholder="<?php esc_attr_e( 'Enter max or left empty', 'calculator-builder' ); ?>"
+                                   autocomplete="off">
                         </div>
                     </div>
                 </div>
                 <div class="column type-number">
                     <div class="field has-addons">
                         <div class="control">
-                            <span class="button is-size-6 is-link">step</span>
+                            <span class="button is-size-6 is-link"><?php esc_html_e( 'step', 'calculator-builder' ); ?></span>
                         </div>
                         <div class="control is-expanded">
-                            <input class="input is-link" name="step" type="text" placeholder="Enter step or left empty"
+                            <input class="input is-link" name="step" type="text"
+                                   placeholder="<?php esc_attr_e( 'Enter max or left empty', 'calculator-builder' ); ?>"
                                    value="0.01" autocomplete="off">
                         </div>
                     </div>
@@ -105,20 +109,24 @@
                 <div class="column">
                     <div class="field has-addons">
                         <div class="control">
-                            <span class="button is-size-6 is-link">min</span>
+                            <span class="button is-size-6 is-link"><?php esc_html_e( 'min', 'calculator-builder' ); ?></span>
                         </div>
                         <div class="control is-expanded">
-                            <input class="input is-link" name="min" type="text" placeholder="Enter min or left empty" autocomplete="off">
+                            <input class="input is-link" name="min" type="text"
+                                   placeholder="<?php esc_attr_e( 'Enter max or left empty', 'calculator-builder' ); ?>"
+                                   autocomplete="off">
                         </div>
                     </div>
                 </div>
                 <div class="column">
                     <div class="field has-addons">
                         <div class="control">
-                            <span class="button is-size-6 is-link">max</span>
+                            <span class="button is-size-6 is-link"><?php esc_html_e( 'max', 'calculator-builder' ); ?></span>
                         </div>
                         <div class="control is-expanded">
-                            <input class="input is-link" type="text" name="max" placeholder="Enter max or left empty" autocomplete="off">
+                            <input class="input is-link" type="text" name="max"
+                                   placeholder="<?php esc_attr_e( 'Enter max or left empty', 'calculator-builder' ); ?>"
+                                   autocomplete="off">
                         </div>
                     </div>
                 </div>
@@ -127,9 +135,10 @@
             <div class="columns type-textarea is-hidden">
                 <div class="column">
                     <div class="field">
-                        <label class="label">Options</label>
+                        <label class="label"><?php esc_html_e( 'Options', 'calculator-builder' ); ?></label>
                         <div class="control">
-                            <textarea class="textarea is-link" name="options" placeholder="name = value"></textarea>
+                            <textarea class="textarea is-link" name="options"
+                                      placeholder="<?php esc_attr_e( 'name = value', 'calculator-builder' ); ?>"></textarea>
                         </div>
                     </div>
                 </div>
@@ -138,19 +147,19 @@
             <div class="columns type-buttons is-hidden">
                 <div class="column">
                     <div class="field">
-                        <label class="label">Button Calculate</label>
+                        <label class="label"><?php esc_html_e( 'Button Calculate', 'calculator-builder' ); ?></label>
                         <div class="control">
-                            <input class="input is-link" type="text" placeholder="Enter text" value="Calculate"
-                                   name="calculate">
+                            <input class="input is-link" type="text" placeholder="Enter text"
+                                   value="<?php esc_attr_e( 'Calculate', 'calculator-builder' ); ?>" name="calculate">
                         </div>
                     </div>
                 </div>
                 <div class="column">
                     <div class="field">
-                        <label class="label">Button Reset</label>
+                        <label class="label"><?php esc_html_e( 'Button Reset', 'calculator-builder' ); ?></label>
                         <div class="control">
                             <input class="input is-link" name="reset" type="text" placeholder="Enter text"
-                                   value="Reset">
+                                   value="<?php esc_attr_e( 'Reset', 'calculator-builder' ); ?>">
                         </div>
                     </div>
                 </div>
@@ -158,8 +167,10 @@
 
             <div class="columns">
                 <div class="column has-text-centered">
-                    <button class="button-add button is-success is-radiusless" name="add-field">Add Field</button>
-                    <button class="button-update button is-success is-hidden is-radiusless" name="update-field">Update Field</button>
+                    <button class="button-add button is-success is-radiusless"
+                            name="add-field"><?php esc_html_e( 'Add Field', 'calculator-builder' ); ?></button>
+                    <button class="button-update button is-success is-hidden is-radiusless"
+                            name="update-field"><?php esc_html_e( 'Update Field', 'calculator-builder' ); ?></button>
                 </div>
             </div>
 
