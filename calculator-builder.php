590c53076bec4c24a0ff8469cfd1e876ef97c513
Dmytro Lobov <givememoney1982@gmail.com>
2023-05-01 15:05:17 +0300

    Update to version 1.4

--- calculator-builder.php
+++ calculator-builder.php
@@ -5,7 +5,7 @@
  * Description:       Easily create Online calculators
  * Author:            CalcHub
  * Author URI:        https://calchub.xyz
- * Version:           1.3.1
+ * Version:           1.4
  * Text Domain:       calculator-builder
  * License:           GPL-2.0+
  * Domain Path:       languages
@@ -81,7 +81,7 @@ if ( ! class_exists( 'Calculator_Builder' ) ) :
 		private function setup_constants() {
 			// Plugin version.
 			if ( ! defined( 'CALCHUB_VERSION' ) ) {
-				define( 'CALCHUB_VERSION', '1.3.1' );
+				define( 'CALCHUB_VERSION', '1.4' );
 			}
 
 			// Plugin Admin slug.
